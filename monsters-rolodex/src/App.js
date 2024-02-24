531cccf0d9ac4834c5c552ed84adc5db8512c2a7
Sriganth V <sriganth3@live.com>
2024-02-24 21:08:47 +0530

    [Monster-Rolodex] cleaned up

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,52 +8,14 @@ class App extends Component{
     super();
 
     this.state = {
-       name: 'John Doe',
-       company: 'FB'
+      
     }; // state is always a json object.
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           
-          <p>
-            Hi {this.state.name}, I work at {this.state.company}
-          </p>
-
-          <button onClick={() => {
-
-            /* 
-            React will not detect this way of changing the and re-render the object
-
-            this.state.name = 'Jane Doe'
-            console.log(this.state.name);
-            */
-
-            /*
-            this.setState({name: 'Jane Doe'});
-            
-            setState is a async call, so while doing console log first time, state will remain same :)
-
-            console.log(this.state);
-            */
-
-            this.setState((state, props) => {
-              return {
-                name: 'Jane Doe'
-              }
-            }, () => {
-              console.log(this.state)
-            })
-
-            
-
-          }}>Change Name</button>
-          {/* EventHandler */}
-
-        </header>
       </div>
     );
   }
